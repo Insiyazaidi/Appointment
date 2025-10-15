b6f170958a43e931465373454854f3bd5b91f5cc
user.name like : Insiya <zaidiinsiya83@gmail.com>
2025-10-16 00:54:31 +0530

    up

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { AppContext } from '../Context/Appcontext'
+import { assets } from '../assets/assets'
 
-const Appointment = () => {
-  return (
-    <div>Appointment</div>
+const Appointment = () => {      {/*  YHA PR EK HI DOCTOR H TOH MAP LGANE KI ZAROORAT NHI HAII  */}
+  const {docId}= useParams()
+  console.log(docId)
+  const {doctors} = useContext(AppContext)
+  const [docinfo , Setdocinfo] = useState(null)
+
+  const fetchDocInfo = async()=>{
+const docinfo = doctors.find(doc=>doc._id===docId)
+Setdocinfo(docinfo)
+console.log(docinfo)
+  }
+
+  useEffect(()=>{
+fetchDocInfo ()
+  } , [doctors , docId])
+
+
+  return docinfo &&  (
+    <div>
+      <div>
+<div>
+  <img src={docinfo.image}></img>
+</div>
+<div>
+  <p>{docinfo.name} <img src={assets.verified_icon}></img></p>
+  <div>
+    <p>{docinfo.degree} - {docinfo.speciality}</p>
+    <button>{docinfo.experience}</button>
+  </div>
+
+</div>
+      </div>
+
+
+
+    </div>
   )
 }
 
